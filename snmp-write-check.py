bbf3b556e026b5dfe5589e4e608c16913f5130ca
root <root@localhost.localdomain>
2019-11-01 17:27:23 -0400

    call snmpwalk and snmpset

--- snmp-write-check.py
+++ snmp-write-check.py
@@ -7,14 +7,16 @@ import shlex
 import subprocess
 from subprocess import PIPE
 
-version = "-v 1"
-community = "-c public"
-target = "xx.xx.xx.xx"
+import re
 
-type_map = {"INTEGER":'i', "unsigned INTEGER":'u', "TIMETICKS":'t', "IPADDRESS":'a', "OBJID":'o',  "STRING":'s', "HEX STRING":'x', "DECIMAL STRING":'d', "BITS":'b', "unsigned int64":'U', "signed int64":'I', "float":'F', "double":'D'}
+version = "-v 2C"
+community = "-c public"
+target = "127.0.0.1"
+
+type_map = {"INTEGER":'i', "unsigned INTEGER":'u', "UNSIGNED":'u', "TIMETICKS":'t', "Timeticks":'t', "IPADDRESS":'a', "OBJID":'o', "OID":'o',  "STRING":'s', "HEX STRING":'x', "Hex-STRING":'x', "DECIMAL STRING":'d', "BITS":'b', "unsigned int64":'U', "signed int64":'I', "float":'F', "double":'D', "NULLOBJ":'n'}
 
 cmd = f"snmpwalk {version} {community} {target}"
-cmd = "cat test/snmpwalk.out"
+#cmd = "cat test/snmpwalk.out"
 
 args = shlex.split(cmd)
 proc = subprocess.Popen(args, stdout=subprocess.PIPE)
@@ -22,22 +24,59 @@ out = proc.stdout.read().decode()
 
 walk = {} 
 for line in out.splitlines():
-    oid = line.split(" = ")[0]
-    type_value = line.split(" = ")[1] #STRING: "DS1515+"
-    type_ = type_map[ type_value.split(": ")[0] ]
-    value = type_value.split(": ")[1]
+    try:
+        oid = line.split(" = ")[0]
+        type_value = line.split(" = ")[1] #STRING: "DS1515+"
+        type_ = type_map[ type_value.split(": ")[0] ]        
+        value = type_value.split(": ")[1]
 
-    walk[oid]= [type_, value]
+        #for TIMETICKS extract only the numeric value
+        if type_ == 't':
+            match = re.search('\((.+?)\)', value)
+            if match:
+                value = match.group(1)
+            else:
+                continue
+        #for HEX STRING put the value in quotes
+        if type_ == 'x':
+            value = f'"{value}"'
+        
+        #store values in a dictionary
+        #walk[oid]= [type_, value]
 
-    #cmd = f"snmpset {version} {community} {oid} {type_} {value} {target}"
-    #args = shlex.split(cmd)
-    #proc = subprocess.Popen(args, stdout=subprocess.PIPE)
-	#retcode = proc.returncode
-    #out = proc.stdout.read().decode()
+        cmd = f"snmpset {version} {community} {target} {oid} {type_} {value}"
+        args = shlex.split(cmd)
+        retcode = subprocess.call(args)
+
+        print(f"{cmd}\n{retcode}\n\n")
+        
+        #Example of FAIL
+        #snmpset -v2c -c public 127.0.0.1 iso.3.6.1.2.1.1.1.0 s "AAA"    
+        #Reason: noAccess
+        #Failed object: iso.3.6.1.2.1.1.1.0
+        #root@kali:~/.snmp/mibs# echo $?
+        #2
+        #...
+        #Reason: notWritable (That object does not support modification)
+        #Failed object: iso.3.6.1.2.1.1.1.0
+        #root@kali:~/.snmp/mibs# echo $?
+        #2
+
+
+        #Example of SUCCESS
+        #root@kali:~/.snmp/mibs# snmpset -v2c -c public 127.0.0.1 iso.3.6.1.2.1.1.5.0 s "AAA"
+        #iso.3.6.1.2.1.1.5.0 = STRING: "AAA"
+        #root@kali:~/.snmp/mibs# echo $?
+        #0
+
+
+    except:
+        pass
 
 
 
-pp.pprint(walk)
+
+#pp.pprint(walk)
 
 
 
